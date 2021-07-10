2af41c57cb954c900e2de00bdf620a17ef751874
speed2CZ <odstrcilpetr@seznam.cz>
2021-07-10 14:45:05 +0200

    Better comment

--- src/LevelData.gd
+++ src/LevelData.gd
@@ -1,4 +1,4 @@
-# Level script, used for setting number of fishes requiredto finish it.
+# Level script, used for setting the number of fishes required to finish the level.
 extends Node2D
 
 export(int, 1, 999) var requiredFishes = 1
@@ -7,5 +7,5 @@ func _ready():
 	if requiredFishes < 1:
 		push_warning("*WARNING: LevelData: Number of required fishes has to be at least 1! ... setting to 1.")
 		requiredFishes = 1
-	
+
 	PlayerData.numRequiredFishes = requiredFishes
